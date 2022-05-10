63f3d0f0007b90b3663f483d20a2ef185fc0f6ca
Omar khairat <omarkhairat133@gmail.com>
2022-05-10 20:45:26 +0200

    priority first edit

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -110,7 +110,7 @@ extern bool thread_mlfqs;
 
 void thread_init (void);
 void thread_start (void);
-
+bool comparePriority(const struct list_elem *first, const struct list_elem *second, void * aux UNUSED);
 void thread_tick (void);
 void thread_print_stats (void);
 
