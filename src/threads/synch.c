12e78d724ce12d1e94748cb81a3e3b5d001c3e54
Omar khairat <omarkhairat133@gmail.com>
2022-05-11 00:41:11 +0200

    trial

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -69,7 +69,7 @@ sema_down (struct semaphore *sema)
   while (sema->value == 0) 
     {
 //      list_push_back (&sema->waiters, &thread_current ()->elem);
-      list_insert_ordered(&sema->waiters, & thread_current ()->elem, & comparePriority, NULL);
+      list_insert_ordered(&sema->waiters, & thread_current ()->elem, comparePriority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -114,10 +114,12 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
-  sema->value++;
+    sema->value++;
+  if (!list_empty (&sema->waiters)) {
+      list_sort(&(sema->waiters), comparePriority, NULL)
+      thread_unblock(list_entry(list_pop_front(&sema->waiters),
+      struct thread, elem));
+  }
   intr_set_level (old_level);
 }
 
